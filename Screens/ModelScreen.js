6028b5207f6899b3db0775f3a85600d5f0845321
yashmitb <yashmitb07@gmail.com>
2025-02-10 23:09:09 -0800

    Fixed icon

--- Screens/ModelScreen.js
+++ Screens/ModelScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import {
   View,
   TextInput,
@@ -8,29 +8,29 @@ import {
   SafeAreaView,
   Animated,
   Text,
-} from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import CropSelect from '../components/CropSelect'; // Adjust the path as necessary
-import { ThemedButton } from 'react-native-really-awesome-button';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import CropSelect from "../components/CropSelect"; // Adjust the path as necessary
+import { ThemedButton } from "react-native-really-awesome-button";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
-const API_KEY = 'b3e96d2d66b34476960e508649ecc65f'; // OpenCage API key
+const API_KEY = "b3e96d2d66b34476960e508649ecc65f"; // OpenCage API key
 const generateDummyReport = () => {
   const dummyData = [
     {
-      crop: 'Cucumbers and gherkins',
+      crop: "Cucumbers and gherkins",
       id: 32,
       predicted_yield: 1692968839179.2405,
     },
-    { crop: 'Tomatoes', id: 112, predicted_yield: 1075851298796.5234 },
+    { crop: "Tomatoes", id: 112, predicted_yield: 1075851298796.5234 },
     {
-      crop: 'Chillies and peppers, green (Capsicum spp. and Pimenta spp.)',
+      crop: "Chillies and peppers, green (Capsicum spp. and Pimenta spp.)",
       id: 26,
       predicted_yield: 806100251198.3422,
     },
-    { crop: 'Strawberries', id: 102, predicted_yield: 195464447066.66428 },
-    { crop: 'Sour cherries', id: 131, predicted_yield: 102580867681.57909 },
+    { crop: "Strawberries", id: 102, predicted_yield: 195464447066.66428 },
+    { crop: "Sour cherries", id: 131, predicted_yield: 102580867681.57909 },
   ];
 
   return {
@@ -41,25 +41,25 @@ const generateDummyReport = () => {
 };
 
 const ModelScreen = () => {
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
   const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
   const [weatherData, setWeatherData] = useState(null);
   const [predictedData, setPredictedData] = useState([]);
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const [selectedCrops, setSelectedCrops] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [modalType, setModalType] = useState(''); // 'success' or 'error'
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalType, setModalType] = useState(""); // 'success' or 'error'
   const navigation = useNavigation(); // Hook to access navigation
 
   const handleCropSelection = (crops) => {
     setSelectedCrops(crops);
-    console.log('Selected Crops: ', crops);
+    console.log("Selected Crops: ", crops);
   };
 
   const handleSearchAndSubmit = async (release) => {
-    if (location.trim() === '') {
-      showModal('error', 'Please enter a ZIP code or city.');
+    if (location.trim() === "") {
+      showModal("error", "Please enter a ZIP code or city.");
       setTimeout(release, 300);
       return;
     }
@@ -76,10 +76,10 @@ const ModelScreen = () => {
       if (data.results.length > 0) {
         const { lat, lng } = data.results[0].geometry;
         setCoordinates({ lat, lng });
-        console.log('Fetched Latitude: ', lat);
-        console.log('Fetched Longitude: ', lng);
+        console.log("Fetched Latitude: ", lat);
+        console.log("Fetched Longitude: ", lng);
 
-        const weatherUrl = `http://yashmitbhaverisetti.pythonanywhere.com/future_weather?latitude=${lat}&longitude=${lng}`;
+        const weatherUrl = `https://yashmitbhaverisetti.pythonanywhere.com/future_weather?latitude=${lat}&longitude=${lng}`;
 
         const weatherResponse = await fetch(weatherUrl);
         if (!weatherResponse.ok) {
@@ -89,7 +89,7 @@ const ModelScreen = () => {
 
         const weatherData = await weatherResponse.json();
         setWeatherData(weatherData);
-        console.log('Weather Data: ', weatherData);
+        console.log("Weather Data: ", weatherData);
 
         const predictionData = {
           temp: weatherData.average_temperature,
@@ -99,11 +99,11 @@ const ModelScreen = () => {
         };
 
         const predictionUrl =
-          'https://yashmitbhaverisetti.pythonanywhere.com/predict';
+          "https://yashmitbhaverisetti.pythonanywhere.com/predict";
         const predictionResponse = await fetch(predictionUrl, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(predictionData),
         });
@@ -115,7 +115,7 @@ const ModelScreen = () => {
 
         const predictedData = await predictionResponse.json();
         setPredictedData(predictedData);
-        console.log('Predicted Data: ', predictedData);
+        console.log("Predicted Data: ", predictedData);
 
         // Store the report along with selected crops in AsyncStorage
         const report = {
@@ -125,26 +125,26 @@ const ModelScreen = () => {
           selectedCrops: selectedCrops, // Store selected crops
         };
 
-        const storedReports = await AsyncStorage.getItem('@cropReports');
+        const storedReports = await AsyncStorage.getItem("@cropReports");
         const reports = storedReports ? JSON.parse(storedReports) : [];
         reports.push(report);
-        await AsyncStorage.setItem('@cropReports', JSON.stringify(reports));
-        console.log('report', report);
+        await AsyncStorage.setItem("@cropReports", JSON.stringify(reports));
+        console.log("report", report);
         // Show success modal after generating the report
-        showModal('success', `Report generated successfully!`);
+        showModal("success", `Report generated successfully!`);
 
         // Navigate to ArchiveScreen with the new report
         setTimeout(() => {
           setModalVisible(false);
-          navigation.navigate('ArchiveScreen', { reports: report }); // Pass the new report
+          navigation.navigate("ArchiveScreen", { reports: report }); // Pass the new report
         }, 2000);
       } else {
         setTimeout(release, 300);
-        showModal('error', 'Location not found.');
+        showModal("error", "Location not found.");
       }
     } catch (error) {
-      console.error('Error:', error);
-      showModal('error', 'Failed to fetch data. Please try again later.');
+      console.error("Error:", error);
+      showModal("error", error);
     }
   };
 
@@ -185,7 +185,9 @@ const ModelScreen = () => {
           onCropSelection={handleCropSelection}
         />
       </Animated.View>
-
+      <Text style={{ marginBottom: "5%", marginTop: "20%" }}>
+        Warning: Based on past data. Use as a reference only.
+      </Text>
       <ThemedButton
         onPress={handleSearchAndSubmit}
         progress
@@ -196,7 +198,8 @@ const ModelScreen = () => {
         borderColor="#0a5952 "
         backgroundDarker="#0d6e65"
         textColor="white"
-        backgroundProgress="#1590bd">
+        backgroundProgress="#1590bd"
+      >
         Search & Submit
       </ThemedButton>
 
@@ -204,13 +207,14 @@ const ModelScreen = () => {
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}>
+        onRequestClose={() => setModalVisible(false)}
+      >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <MaterialCommunityIcons
-              name={modalType === 'success' ? 'check-circle' : 'alert-circle'}
+              name={modalType === "success" ? "check-circle" : "alert-circle"}
               size={48}
-              color={modalType === 'success' ? 'green' : 'red'}
+              color={modalType === "success" ? "green" : "red"}
             />
             <Text style={styles.modalText}>{modalMessage}</Text>
 
@@ -223,7 +227,8 @@ const ModelScreen = () => {
               borderColor="#0a5952 "
               backgroundDarker="#0d6e65"
               textColor="white"
-              backgroundProgress="#1590bd">
+              backgroundProgress="#1590bd"
+            >
               Close
             </ThemedButton>
           </View>
@@ -236,68 +241,68 @@ const ModelScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#f7f7f7',
+    alignItems: "center",
+    backgroundColor: "#f7f7f7",
   },
   titleText: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    width: '100%',
+    fontWeight: "bold",
+    textAlign: "center",
+    width: "100%",
   },
   inputLabel: {
     fontSize: 28,
-    width: '100%',
-    color: '#555',
+    width: "100%",
+    color: "#555",
     marginBottom: 10,
-    marginTop: '10%',
+    marginTop: "10%",
   },
   searchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '90%',
+    flexDirection: "row",
+    alignItems: "center",
+    width: "90%",
     borderWidth: 2,
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    backgroundColor: "#fff",
     borderRadius: 30,
-    borderColor: '#d3d3d3',
+    borderColor: "#d3d3d3",
     elevation: 5, // Matching elevation
-    shadowColor: '#000', // Same shadow color
+    shadowColor: "#000", // Same shadow color
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.2,
     shadowRadius: 2,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   input: {
     flex: 1,
     height: 65,
     borderRadius: 55,
     paddingHorizontal: 20,
-    backgroundColor: '#fff',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    textAlign: "center",
     fontSize: 30,
-    textColor: '#858585',
+    textColor: "#858585",
   },
   submitButton: {
     marginVertical: 0,
   },
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
     width: 300,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   modalText: {
     fontSize: 18,
     marginVertical: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
   modalButton: {
     marginTop: 15,
